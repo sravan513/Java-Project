92c819a3443e21af190b45b719a818020555041d
sravan513 <sravan.06513@gmail.com>
2024-09-23 10:59:34 +0530

    AddNum modified code

--- AddNum.java
+++ AddNum.java
@@ -4,9 +4,13 @@ class AddNum {
       
       int first = 10;
       int second = 20;
+      int third  = 40;
+      int fourth = 60;
   
       // add two numbers
       int sum = first + second;
+      int sum2 = third + fourth;
       System.out.println(first + " + " + second + " = "  + sum);
+      System.out.println(third + " + " + fourth + " = "  + sum2);
     }
   }
